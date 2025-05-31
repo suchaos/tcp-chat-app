78b4e3f4e2d6670da2b115a2f334ca79909c24e4
suchao <822075222@qq.com>
2025-05-31 18:18:06 +0800

    feat: 使用flag包增加启动参数

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
-import "demo/client"
+import (
+	"demo/client"
+	"demo/server"
+	"flag"
+	"fmt"
+)
 
 func main() {
-	//tcpServer := server.New(8080)
-	//tcpServer.Start()
+	mode := flag.String("mode", "server", "运行模式: server or client")
+	port := flag.Int("port", 8080, "监听或者连接的端口号")
 
-	tcpClient := client.New(8080)
-	tcpClient.Start()
+	flag.Parse()
+
+	switch *mode {
+	case "server":
+		tcpServer := server.New(*port)
+		tcpServer.Start()
+	case "client":
+		tcpClient := client.New(*port)
+		tcpClient.Start()
+	default:
+		fmt.Printf("未知模式 %s, 请指定正确的运行模式", *mode)
+		flag.Usage()
+	}
 }
